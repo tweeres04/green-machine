326d540581db2ad1dc6f71304a08f0609ed894da
Tyler Weeres <tweeres04@gmail.com>
2024-07-26 12:25:27 -0700

    Preserve edit mode when adding a goal

--- app/routes/players.$playerId.goals.ts
+++ app/routes/players.$playerId.goals.ts
@@ -2,7 +2,7 @@ import { ActionFunctionArgs, redirect } from '@remix-run/node'
 import { getDb } from '~/lib/getDb'
 import { goldenBootEntries } from '~/schema'
 
-export async function action({ params }: ActionFunctionArgs) {
+export async function action({ request, params }: ActionFunctionArgs) {
 	const db = getDb()
 
 	await db.insert(goldenBootEntries).values({
@@ -11,5 +11,8 @@ export async function action({ params }: ActionFunctionArgs) {
 		playerId: Number(params.playerId),
 	})
 
-	return redirect('/')
+	const referrer = new URL(request.headers.get('referer') ?? '/')
+	const newPath = `${referrer.pathname}${referrer.search}`
+
+	return redirect(newPath)
 }
