4f85502de9c6657d6c079d0dcc5bcd2b3ec3b7cd
Tyler Weeres <tweeres04@gmail.com>
2024-10-30 07:50:20 -0700

    Player invites

---
+++ app/routes/invites.$inviteId.tsx
@@ -0,0 +1,87 @@
+import { json, LoaderFunctionArgs, redirect } from '@remix-run/node'
+import { Link, useLoaderData } from '@remix-run/react'
+import { eq } from 'drizzle-orm'
+import invariant from 'tiny-invariant'
+import { Button } from '~/components/ui/button'
+import Nav from '~/components/ui/nav'
+import { authenticator } from '~/lib/auth.server'
+import { getDb } from '~/lib/getDb'
+import { commitSession, getSession } from '~/lib/session.server'
+import { userInvites } from '~/schema'
+
+export async function loader({ params, request }: LoaderFunctionArgs) {
+	invariant(params.inviteId, 'Missing inviteId parameter')
+
+	const db = getDb()
+
+	const inviteId = Number(params.inviteId)
+
+	const invite = await db.query.userInvites.findFirst({
+		where: (userInvites, { eq }) => eq(userInvites.id, inviteId),
+		with: {
+			inviter: true,
+			player: {
+				with: {
+					team: true,
+				},
+			},
+		},
+	})
+
+	if (!invite) {
+		throw new Response(null, { status: 404 })
+	}
+
+	const user = await authenticator.isAuthenticated(request)
+
+	if (invite.userId === user?.id) {
+		return json({ team: invite.player.team, inviterName: invite.inviter.name })
+	}
+
+	const token = new URL(request.url).searchParams.get('token')
+
+	// todo token expiry
+	if (!token || token !== invite.token) {
+		throw new Response(null, { status: 401 })
+	}
+
+	if (!user) {
+		const session = await getSession(request.headers.get('Cookie'))
+
+		session.set('inviteId', inviteId)
+		session.set('inviteToken', token)
+
+		return redirect('/login', {
+			headers: {
+				'Set-Cookie': await commitSession(session),
+			},
+		})
+	}
+
+	await db
+		.update(userInvites)
+		.set({
+			userId: user.id,
+			acceptedAt: new Date().toISOString(),
+		})
+		.where(eq(userInvites.id, inviteId))
+
+	return json({ team: invite.player.team, inviterName: invite.inviter.name })
+}
+
+export default function Invite() {
+	const { inviterName, team } = useLoaderData<typeof loader>()
+	return (
+		<>
+			<Nav title="Invite" team={team} />
+			<p>
+				You've accepted {inviterName}'s invite to {team.name}!
+			</p>
+			<div className="text-center">
+				<Button asChild variant="link">
+					<Link to={`/${team.slug}`}>{team.name} home</Link>
+				</Button>
+			</div>
+		</>
+	)
+}
--- app/routes/teams.$teamId.players.ts
+++ app/routes/teams.$teamId.players.ts
@@ -1,7 +1,55 @@
 import { ActionFunctionArgs } from '@remix-run/node'
 import invariant from 'tiny-invariant'
+import { authenticator, hasAccessToTeam } from '~/lib/auth.server'
 import { getDb } from '~/lib/getDb'
-import { players } from '~/schema'
+import { players, userInvites } from '~/schema'
+import { randomBytes } from 'node:crypto'
+
+import Mailgun from 'mailgun.js'
+
+async function sendInviteEmail({
+	teamId,
+	inviterName,
+	email,
+	inviteId,
+	randomToken,
+}: {
+	teamId: string
+	inviterName: string
+	email: string
+	inviteId: number
+	randomToken: string
+}) {
+	invariant(process.env.MAILGUN_API_KEY, 'No MAILGUN_API_KEY')
+	invariant(process.env.MAILGUN_DOMAIN, 'No MAILGUN_DOMAIN')
+	invariant(process.env.BASE_URL, 'No BASE_URL')
+
+	const db = getDb()
+
+	const teamRecord = await db.query.teams.findFirst({
+		where: (teams, { eq }) => eq(teams.id, Number(teamId)),
+		columns: {
+			name: true,
+		},
+	})
+
+	invariant(teamRecord, 'Team not found')
+
+	const teamName = teamRecord.name
+
+	const mailgun = new Mailgun(FormData)
+	const mg = mailgun.client({
+		username: 'api',
+		key: process.env.MAILGUN_API_KEY,
+	})
+
+	return mg.messages.create(process.env.MAILGUN_DOMAIN, {
+		from: 'TeamStats Invite <invites@teamstats.tweeres.com>',
+		to: email,
+		subject: `${inviterName} invited you to join ${teamName} on TeamStats`,
+		text: `Accept your invite here: ${process.env.BASE_URL}/invites/${inviteId}?token=${randomToken}`,
+	})
+}
 
 export async function action({
 	request,
@@ -9,14 +57,60 @@ export async function action({
 }: ActionFunctionArgs) {
 	invariant(teamId, 'Missing teamId parameter')
 
+	const user = await authenticator.isAuthenticated(request)
+
+	if (!user) {
+		throw new Response(null, { status: 401 })
+	}
+
 	const db = getDb()
+
+	const userHasAccessToTeam = await hasAccessToTeam(user, Number(teamId))
+
+	if (!userHasAccessToTeam) {
+		throw new Response(null, { status: 403 })
+	}
 
 	const formData = await request.formData()
 	const name = formData.get('name')
+	const email = formData.get('email')
 
 	if (typeof name !== 'string') {
 		throw new Response('Name is required', { status: 400 })
 	}
 
-	return db.insert(players).values({ name, teamId: Number(teamId) })
+	const randomToken = randomBytes(16).toString('hex')
+
+	return db.transaction(async (tx) => {
+		const newPlayers = await tx
+			.insert(players)
+			.values({ name, teamId: Number(teamId) })
+			.returning()
+		const newPlayer = newPlayers[0]
+
+		if (email && typeof email === 'string') {
+			const insertedInvites = await tx
+				.insert(userInvites)
+				.values({
+					email,
+					playerId: newPlayer.id,
+					createdAt: new Date().toISOString(),
+					token: randomToken,
+					inviterId: user.id,
+				})
+				.returning({ inviteId: userInvites.id })
+
+			const { inviteId } = insertedInvites[0]
+
+			sendInviteEmail({
+				teamId,
+				inviteId,
+				inviterName: user.name,
+				email,
+				randomToken,
+			})
+		}
+
+		return null
+	})
 }
