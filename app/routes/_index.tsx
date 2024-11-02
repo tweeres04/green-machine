bbc7d3c050e641a54c385738918da024cfa55b44
Tyler Weeres <tweeres04@gmail.com>
2024-11-02 09:29:04 -0700

    Fix stats on home page

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,9 +1,9 @@
 import type { LoaderFunctionArgs, MetaFunction } from '@remix-run/node'
-import { Form, json, useLoaderData } from '@remix-run/react'
+import { Await, defer, Form, useLoaderData } from '@remix-run/react'
 import { Input } from '~/components/ui/input'
 import { Button } from '~/components/ui/button'
 import { kebabCase } from 'lodash-es'
-import React from 'react'
+import React, { Suspense } from 'react'
 import { authenticator } from '~/lib/auth.server'
 import { getDb } from '~/lib/getDb'
 import Nav from '~/components/ui/nav'
@@ -15,6 +15,7 @@ import {
 	DialogTrigger,
 	DialogContent,
 } from '~/components/ui/dialog'
+import invariant from 'tiny-invariant'
 
 export const meta: MetaFunction = () => {
 	return [
@@ -103,26 +104,45 @@ export async function loader({ request }: LoaderFunctionArgs) {
 	const db = getDb()
 
 	const sql_ = sql`
-		select
+		select distinct
 			teams.id,
 			teams.name,
-			teams.slug,
-			count(players.id) playerCount,
-			count(stat_entries.id) statCount
+			teams.slug
 		from teams
-			inner join users_teams on teams.id = users_teams.team_id
-			inner join players players_for_users on teams.id = players.team_id
-			inner join user_invites on user_invites.player_id = players_for_users.id
-			left join players on teams.id = players.team_id
-			left join stat_entries on players.id = stat_entries.player_id
+			left join users_teams on teams.id = users_teams.team_id
+			left join players players_for_users on teams.id = players_for_users.team_id
+			left join user_invites on user_invites.player_id = players_for_users.id
 		where
 			users_teams.user_id = ${user.id} or user_invites.user_id = ${user.id}
-		group by teams.id
+		order by teams.name
 	`
 
 	const teams_ = await db.all(sql_)
 
-	return json({ teams: teams_ })
+	const teamIds = teams_.map((t) => t.id)
+
+	const statsPromise = db.all(sql`
+		select
+			teams.id,
+			teams.name,
+			(
+				select count(*)
+				from stat_entries
+					inner join players on stat_entries.player_id = players.id
+				where team_id = teams.id
+			) statCount,
+			(
+				select count(*)
+				from players
+				where team_id = teams.id
+			) playerCount
+		from teams
+			inner join players on teams.id = players.team_id
+		where
+			teams.id in ${teamIds}
+	`)
+
+	return defer({ teams: teams_, stats: statsPromise })
 }
 
 type Team = {
@@ -133,8 +153,14 @@ type Team = {
 	statCount: number
 }
 
+type Stats = {
+	id: number
+	playerCount: number
+	statCount: number
+}
+
 export default function Index() {
-	const { teams } = useLoaderData<{ teams: Team[] }>()
+	const { teams, stats } = useLoaderData<{ teams: Team[]; stats: Stats[] }>()
 
 	return (
 		<div className="max-w-[700px] mx-auto space-y-8 p-2">
@@ -149,10 +175,22 @@ export default function Index() {
 										{t.name}
 									</a>
 								</Button>
-								<p>
-									{t.playerCount} player{t.playerCount !== 1 && 's'},{' '}
-									{t.statCount} stat{t.statCount !== 1 && 's'} recorded
-								</p>
+								<Suspense>
+									<Await resolve={stats}>
+										{(stats) => {
+											const statsForTeam = stats.find((s) => s.id === t.id)
+											invariant(statsForTeam, 'No statsForTeam')
+											return (
+												<p>
+													{statsForTeam.playerCount} player
+													{statsForTeam.playerCount !== 1 && 's'},{' '}
+													{statsForTeam.statCount} stat
+													{statsForTeam.statCount !== 1 && 's'} recorded
+												</p>
+											)
+										}}
+									</Await>
+								</Suspense>
 							</li>
 						)
 					})}
