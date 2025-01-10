ed93e4ed538862d75f0987826297e989e2778e05
Tyler Weeres <tweeres04@gmail.com>
2025-01-10 08:09:47 -0800

    Set favicon

--- app/root.tsx
+++ app/root.tsx
@@ -59,6 +59,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
 				<meta charSet="utf-8" />
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
 				<link rel="manifest" href="/manifest.json" />
+				<link rel="icon" href="/formation.png" type="image/png" />
 				<Meta />
 				<Links />
 				<style
