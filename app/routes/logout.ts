dd582e2e5f9ed138ae8b616878991734df2baaf1
Tyler Weeres <tweeres04@gmail.com>
2025-01-23 07:34:57 -0800

    Redirect to the homepage when you log out

--- app/routes/logout.ts
+++ app/routes/logout.ts
@@ -2,5 +2,5 @@ import { ActionFunctionArgs } from '@remix-run/node'
 import { authenticator } from '~/lib/auth.server'
 
 export async function loader({ request }: ActionFunctionArgs) {
-	await authenticator.logout(request, { redirectTo: '/login' })
+	await authenticator.logout(request, { redirectTo: '/' })
 }
