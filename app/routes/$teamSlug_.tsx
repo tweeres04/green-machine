6e1c2bce618fbd989262afc9c7b345ffc5490f62
Tyler Weeres <tweeres04@gmail.com>
2025-01-31 09:05:42 -0800

    Stats screen metadata tweak

--- app/routes/$teamSlug_.tsx
+++ app/routes/$teamSlug_.tsx
@@ -69,7 +69,7 @@ export const meta: MetaFunction = ({ data }: MetaArgs) => {
 	} = data as { team: Team }
 
 	const title = `${name} - TeamStats`
-	const description = `Team stats for ${name}. Add goals and assists for each player. Shareable standings.`
+	const description = `Team stats for ${name}. Track goals and assists for each player. Shareable standings.`
 	const url = `https://teamstats.tweeres.com/${slug}`
 
 	return [
